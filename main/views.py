ea571e2f81021026764d120a0890951a177f78b8
malikaatha <malika.atha@ui.ac.id>
2024-11-13 14:38:36 +0700

    PUSH T8

--- main/views.py
+++ main/views.py
@@ -1,28 +1,31 @@
-import datetime
-from django.http import HttpResponseRedirect
-from django.urls import reverse
-from django.http import HttpResponse
-from django.core import serializers
 from django.shortcuts import render, redirect
 from main.forms import MoodEntryForm
 from main.models import MoodEntry
+from django.http import HttpResponse
+from django.core import serializers
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import authenticate, login
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
+import datetime
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.shortcuts import reverse
+from django.http import HttpResponseRedirect
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 from django.utils.html import strip_tags
 
-# Create your views here.
 @login_required(login_url='/login')
+# Create your views here.
 def show_main(request):
+
     context = {
+        'npm' : '2306245794',
         'name': request.user.username,
-        'class': 'PBP A',
-        'npm': '2306275203',
+        'class': 'PBP F',
         'last_login': request.COOKIES['last_login'],
     }
 
@@ -70,15 +73,15 @@ def register(request):
 
 def login_user(request):
    if request.method == 'POST':
-        form = AuthenticationForm(data=request.POST)
+      form = AuthenticationForm(data=request.POST)
 
-        if form.is_valid():
+      if form.is_valid():
             user = form.get_user()
             login(request, user)
             response = HttpResponseRedirect(reverse("main:show_main"))
             response.set_cookie('last_login', str(datetime.datetime.now()))
             return response
-        else:
+      else:
             messages.error(request, "Invalid username or password. Please try again.")
 
    else:
